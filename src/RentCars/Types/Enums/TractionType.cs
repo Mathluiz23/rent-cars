595c57323019723a94c3170829ca8ac4b5c29f31
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-04 16:49:37 -0300

    :sparkles: feat: Implementing enum TRactionType, inserting the values of attributes about wheel cars

--- src/RentCars/Types/Enums/TractionType.cs
+++ src/RentCars/Types/Enums/TractionType.cs
@@ -1,6 +1,10 @@
 namespace RentCars.Types
+
 {
-    public enum TractionType
-    {
-    }
+  public enum TractionType
+  {
+    FrontWheelDrive = 0,
+    RearWheelDrive = 1,
+    AllWheelDrive = 2
+  }
 }
