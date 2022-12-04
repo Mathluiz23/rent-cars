3e956def5ef1f4fbefb484779e9e2051b23181ac
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-04 16:23:09 -0300

    :sparkles: feat: Implementing struct to colors, inserting public attributes, name and hex how string

--- src/RentCars/Types/Structs/Color.cs
+++ src/RentCars/Types/Structs/Color.cs
@@ -1,5 +1,16 @@
 namespace RentCars.Types;
 
+// A struct deve conter o campo Name do tipo string
+// A struct deve conter o campo Hex do tipo string
+
 public struct Color
 {
+  public string Name { get; set; }
+  public string Hex { get; set; }
+
+  public Color(string name, string hex)
+  {
+    Name = name;
+    Hex = hex;
+  }
 }
