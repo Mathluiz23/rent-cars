939483313de952ff768da646fc61dd9c2c9ce57f
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-04 17:15:14 -0300

    :sparkles: feat: Implementing the Rent model, following business rules for car rentals

--- src/RentCars/Models/Rent.cs
+++ src/RentCars/Models/Rent.cs
@@ -4,24 +4,30 @@ namespace RentCars.Models;
 
 public class Rent
 {
-    public Vehicle Vehicle { get; set; }
-    public Person Person { get; set; }
-    public int DaysRented { get; set; }
-    public double Price { get; set; }
-    public RentStatus Status { get; set; }
+  public Vehicle Vehicle { get; set; }
+  public Person Person { get; set; }
+  public int DaysRented { get; set; }
+  public double Price { get; set; }
+  public RentStatus Status { get; set; }
 
-    public Rent(Vehicle vehicle, Person person, int daysRented)
-    {
-        throw new NotImplementedException();
-    }
+  public Rent(Vehicle vehicle, Person person, int daysRented)
+  {
+    Vehicle = vehicle;
+    Person = person;
+    DaysRented = daysRented;
+    Price = person is PhysicalPerson ? vehicle.PricePerDay * daysRented : vehicle.PricePerDay * daysRented * 0.9;
+    Status = RentStatus.Confirmed;
+    Vehicle.IsRented = true;
+    Person.Debit = Price;
+  }
 
-    public void Cancel()
-    {        
-        throw new NotImplementedException();
-    }
+  public void Cancel()
+  {
+    throw new NotImplementedException();
+  }
 
-    public void Finish()
-    {        
-        throw new NotImplementedException();
-    }
+  public void Finish()
+  {
+    throw new NotImplementedException();
+  }
 }
