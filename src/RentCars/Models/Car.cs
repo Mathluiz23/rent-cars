b84b8d85028adf69483a0db86eec9a9290feaf02
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-22 11:01:21 -0300

    :book: deleting unnecessary comments

--- src/RentCars/Models/Car.cs
+++ src/RentCars/Models/Car.cs
@@ -4,9 +4,9 @@ namespace RentCars.Models;
 
 public class Car : Vehicle
 {
-    public int Seats { get; set; }
-    public int Doors { get; set; }
-    public TractionType Traction;
-    public BrakeType FrontBrake { get; set; }
-    public BrakeType RearBrake { get; set; }
+  public int Seats { get; set; }
+  public int Doors { get; set; }
+  public TractionType Traction;
+  public BrakeType FrontBrake { get; set; }
+  public BrakeType RearBrake { get; set; }
 }
--- src/RentCars/Types/Enums/BrakeType.cs
+++ src/RentCars/Types/Enums/BrakeType.cs
@@ -1,11 +1,5 @@
 namespace RentCars.Types;
 
-// Crie sua lógica em src/RentCars/Types/Enums/BreakeType.cs
-
-// O enum deve conter o campo Chamber com valor 1
-// O enum deve conter o campo Disc com valor 2
-// O enum deve conter o campo Drum com valor 3
-
 public enum BrakeType
 {
   Chamber = 1,
